234d889c67f6bb69fde6e3cb6e534168a6ee496d
Ces <ces@Zipcoders-MacBook-Pro-42.local>
2023-11-11 13:13:59 -0500

    Checkpointing my Work

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,15 +4,43 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
+
+    Character[][] matrix;
     public Board(Character[][] matrix) {
+
+        this.matrix = matrix;
     }
 
     public Boolean isInFavorOfX() {
-        return null;
+
+        return checkRow('X') || checkColumn('X');
     }
 
+
+    public boolean checkRow(Character letter) {
+
+        for (int i = 0; i < 3; i++) {
+            if (this.matrix[i][0] == letter && this.matrix[i][1] == letter && this.matrix[i][2] == letter) {
+                return true;
+            }
+        } return false;
+    }
+
+    public boolean checkColumn(Character letter) {
+        for (int i = 0; i <3; i++) {
+            if (this.matrix[0][i] == letter && this.matrix [1][i] == letter && this.matrix[2][i] == letter) {
+                return true;
+            }
+
+            } return false;
+        }
+
+//    public boolean checkDiagonal() {
+//
+//    }
+
     public Boolean isInFavorOfO() {
-        return null;
+        return checkRow('O') || checkColumn('O');
     }
 
     public Boolean isTie() {
